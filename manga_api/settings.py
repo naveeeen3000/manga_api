f891e3ee9535c2e0052671432b6b7853efb1b936
naveeeeeeen <naveen.s1@practo.com>
2022-07-11 18:29:37 +0530

    added static files

--- manga_api/settings.py
+++ manga_api/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'manga_api.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': str(BASE_DIR / 'db.sqlite3'),
     }
 }
 
@@ -118,7 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = 'static'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
