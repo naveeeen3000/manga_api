c3d60178126f74471dd6a5c9194dc1c65d8611d2
Naveen <naveen@Naveens-MacBook-Air.local>
2024-07-14 22:37:43 +0530

    added health check

--- manga_api/urls.py
+++ manga_api/urls.py
@@ -1,19 +1,21 @@
 
 from django.contrib import admin
 from django.urls import path,include
-from django.contrib.auth.models import User
-from rest_framework import routers, serializers, viewsets
-from django.db import models
+from django.http.response import JsonResponse
 from . import views
 # from rest_framework.authtoken import views
+
+def health_check(request):
+    """health check"""
+    return JsonResponse(data={'message': "up and running"})
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.IndexView.as_view(),name='index'),
     path('api/v1/',include("api.urls")),
     path('api-auth/', include('rest_framework.urls')),
-    path('accounts/',include('accounts.urls'))
-
+    path('accounts/',include('accounts.urls')),
+    path('health/check/', health_check)
 ]
 
 
