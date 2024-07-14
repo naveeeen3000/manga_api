d50043c887596bfe42abdfd11b79dcf735072717
Naveen <naveen@Naveens-MacBook-Air.local>
2024-07-14 22:43:27 +0530

    temp fix for getting site up

--- manga_api/settings.py
+++ manga_api/settings.py
@@ -8,10 +8,7 @@ SECRET_KEY = config('DJANGO_SECRET_KEY')
 DEBUG = to_bool(config("DEBUG"))
 
 ALLOWED_HOSTS = [
-    'api.naveenkumar.world',
-    'naveenkumar.world'
-    'https://naveenkumar.world',
-    'https://api.naveenkumar.world'
+    '*'
     ]
 
 INSTALLED_APPS = [
